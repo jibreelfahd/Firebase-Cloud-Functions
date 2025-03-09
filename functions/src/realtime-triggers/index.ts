ea4db37f847578eb8c24d7f2cb5124a591961194
Fahd Jibril <jibreelfahd@gmail.com>
2025-03-09 23:04:21 +0100

    add realtime db code sample

--- functions/src/realtime-triggers/index.ts
+++ functions/src/realtime-triggers/index.ts
@@ -1,1 +1,9 @@
-// import {onValueCreated} from "firebase-functions/v2/database";
+import {onValueCreated} from "firebase-functions/v2/database";
+import {logger} from "firebase-functions";
+
+
+export const onDataCreated = onValueCreated("users/{uid}", (event) => {
+  const dataRecieved = event.data.val();
+
+  logger.log("Event Created Succesfully", dataRecieved);
+});
