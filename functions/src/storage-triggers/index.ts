86f770053e5e429fa83028c1d7aedc62bf779a17
Fahd Jibril <jibreelfahd@gmail.com>
2025-03-14 22:31:13 +0100

    add another storage trigger

--- functions/src/storage-triggers/index.ts
+++ functions/src/storage-triggers/index.ts
@@ -1,7 +1,12 @@
-import {onObjectFinalized} from "firebase-functions/v2/storage";
+import {onObjectFinalized, 
+  onObjectArchived} from "firebase-functions/v2/storage";
 import {logger} from "firebase-functions/v2";
 
 export const onFinalized = onObjectFinalized((event) => {
   const data = event.data;
   logger.info("This is data", data);
+});
+
+export const onArchive = onObjectArchived((event) => {
+  logger.log("Event object", event);
 });
